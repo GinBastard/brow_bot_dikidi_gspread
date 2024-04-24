ccf8ad87ea8e69082992620a1d2102ae2934d0cf
GinBastard <riehkala2016@gmail.com>
2024-04-24 22:46:21 +0300

    5454

--- tg_bot.py
+++ tg_bot.py
@@ -15,6 +15,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
     # Для простых тестов и отладки вы можете использовать MemoryStorage,
     # который хранит состояния и данные в памяти. Однако, на боевом сервере для продакшена лучше использовать другие хранилища, такие как RedisStorage.
@@ -29,7 +30,7 @@ API_TOKEN = '7141747698:AAHu-H6Z7w3Jm8kIvjc_6XaGyLzN5bK2x54'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
-
+dp.middleware.setup(LoggingMiddleware())
 
 
 procedures = ['Межресничка', 'Волоски', 'Гиперреализм', 'Пудровое']
@@ -45,8 +46,7 @@ class DateTimeSelection(StatesGroup):
     date_time = State()
 
 class PhoneNumberSelection(StatesGroup):
-    waiting_for_phone = State()
-
+    phone = State()
 
 
 def create_keyboard_procedures(procedures):
@@ -59,20 +59,20 @@ def create_keyboard_places(places):
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(*buttons)
     return keyboard
 
-async def create_keyboard_dates_and_times(df_result,  state: FSMContext):
-    dt = []
-    dates = df_result.index
-    for date in dates:
-        times = [time for time in df_result.loc[date] if time != '']
-        if times:
-            for time in times:
-                dt.append(f"{date} {time}")
+def create_keyboard_dates_and_times(dt):
     buttons = [KeyboardButton(text) for text in dt]
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(*buttons)
-    await state.update_data(dt=dt)
     return keyboard
 
-#df_result = None
+def create_keyboard_get_phone():
+    button = types.KeyboardButton("Отправить номер телефона", request_contact=True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(button)
+    return keyboard
+
+
+dt = []       # список для кнопок выбора даты и времени
+
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
@@ -84,22 +84,22 @@ async def start(message: types.Message):
     # Например, сохранение в глобальной переменной или базе данных
 
     await message.answer(f"Привет, {user_name}!\nЯ помогу Вам записаться на прием к мастеру Веронике.")
-
+    await start2(bot, message)
 @dp.message_handler()
-async def start2(message: types.Message):
+async def start2(bot, message: types.Message):
     keyboard_procedures = create_keyboard_procedures(procedures)
-    await message.answer("Пожалуйста, выберите процедуру", reply_markup=keyboard_procedures)
+    await bot.send_message(message.chat.id, "Пожалуйста, выберите процедуру", reply_markup=keyboard_procedures)
     await ProcedureSelection.procedure.set()
 
 
 @dp.message_handler(state=ProcedureSelection.procedure)
 async def process_procedure_choice(message: types.Message, state: FSMContext):
     global selected_procedure
-    selected_procedure = message.text  # выбранная процедура!!
 
-    if selected_procedure not in procedures:
+    if message.text not in procedures:
         await message.answer("Пожалуйста, выберите процедуру из списка кнопок.")
-        return
+    else:
+        selected_procedure = message.text
 
     keyboard_places = create_keyboard_places(places)
     await message.answer(f"Выбрана процедура: {selected_procedure}. Выберите место", reply_markup=keyboard_places)
@@ -109,68 +109,90 @@ async def process_procedure_choice(message: types.Message, state: FSMContext):
 @dp.message_handler(state=PlaceSelection.place)
 async def process_place_choice(message: types.Message, state: FSMContext):
     global selected_place
-    selected_place = message.text
 
-    if selected_place not in places:
+    if message.text not in places:
         await message.answer("Пожалуйста, выберите место из списка кнопок.")
-        return
+    else:
+        selected_place = message.text
+
 
     # Здесь вызовите вашу функцию для запуска процесса
     await message.answer(f"Выбрано место: {selected_place}. \nСейчас сверю график и вернусь к вам (в теч. 15-30 сек.), выберем дату и время процедуры.", reply_markup=types.ReplyKeyboardRemove())
     await state.update_data(selected_place=selected_place)
-    await DateTimeSelection.date_time.set()
+
 
     df_result = result.result_date_time(selected_procedure, selected_place)  # создаем df_result - в файле result_date_time.py
 
-    keyboard_dates_and_times = create_keyboard_dates_and_times(df_result, state=)
+    # Заполняем dt из df_result
+    dates = df_result.index
+    for date in dates:
+        times = [time for time in df_result.loc[date] if time != '']
+        if times:
+            for time in times:
+                dt.append(f"{date} {time}")
 
-    await message.answer("Спасибо за ожидание.\nВыберите дату и время:", reply_markup=keyboard_dates_and_times)
-    await state.update_data(df_result=df_result)
-
-
+    keyboard_dates_and_times = create_keyboard_dates_and_times(dt)
+    await message.answer(f"Спасибо за ожидание.\nВыберите дату и время", reply_markup=keyboard_dates_and_times)
+    await DateTimeSelection.date_time.set()
 
 @dp.message_handler(state=DateTimeSelection.date_time)
 async def process_date_time_choice(message: types.Message, state: FSMContext):
     global selected_date_time
-    #selected_date_time = message.text
-    data = await state.get_data()
-    df_result = data.get('df_result')
-    dt = data.get('dt')
-    # Получение доступных дат и времени из датафрейма df_result
 
-    if df_result is not None:
-        dates = df_result.index
-        times = df_result.columns
-
-    # Проверка, что сообщение пользователя соответствует одной из доступных дат и времени
-    if message.text in dt:
-        selected_date_time = message.text
-    else:
+    # Проверка, соответствует ли ввод пользователя одному из значений списка dt
+    if message.text not in dt:
         await message.answer("Пожалуйста, выберите дату и время из списка кнопок.")
-        return
+    else:
+        selected_date_time = message.text
 
-    await message.answer(f"Выбраны дата и время: {selected_date_time}.\nЯ отправлю Вашу запись Веронике и она свяжется с Вами для подтверждения в ближайшее время.",reply_markup=types.ReplyKeyboardRemove())
+    keyboard = create_keyboard_get_phone()
+    await message.answer(f"Выбраны дата и время: {selected_date_time}.\nДля того, чтобы можно было подтвердить запись, пожалуйста, отправьте свой номер телефона, нажав на кнопку ниже.", reply_markup=keyboard)
     await state.update_data(selected_date_time=selected_date_time)
-    # await message.answer(
-    #     f"Выбранные дата и время: {selected_date_time}. \nСпасибо. Я отправлю Вашу запись Веронике и она свяжется с Вами для подтверждения в ближайшее время.",reply_markup=types.ReplyKeyboardRemove())
+    await PhoneNumberSelection.phone.set()
 
-@dp.message_handler(commands=['get_phone'])
-async def get_phone_number(message: types.Message):
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = types.KeyboardButton("Отправить номер телефона", request_contact=True)
-        keyboard.add(button)
-
-        await message.answer("Для того, чтобы можно было подтвердить запись, пожалуйста, отправьте свой номер телефона, нажав на кнопку ниже.", reply_markup=keyboard)
-        await PhoneNumberSelection.waiting_for_phone.set()
-
-@dp.message_handler(content_types=types.ContentType.CONTACT, state=PhoneNumberSelection.waiting_for_phone)
+@dp.message_handler(content_types=types.ContentType.CONTACT, state=PhoneNumberSelection.phone)
 async def process_phone_number(message: types.Message, state: FSMContext):
-        user_phone = message.contact.phone_number
-        # Сохранение номера телефона в глобальной переменной или базе данных
+    global user_phone
+    user_phone = message.contact.phone_number
+    # Сохранение номера телефона в глобальной переменной или базе данных
+    await message.answer(f"Спасибо!\nВероника свяжется с Вами для подтверждения записи в ближайшее время.\nХорошего дня!", reply_markup=types.ReplyKeyboardRemove())
+    await state.update_data(user_phone=user_phone)
 
-        await message.answer(f"Спасибо за предоставленный номер телефона: {user_phone}")
 
+    print(selected_procedure)
+    print(selected_place)
+    print(selected_date_time)
+    print(user_phone)
+
+    # Отправка сообщения пользователю с командой /reset
+    await message.answer("Нажмите /reset, чтобы сбросить настройки.", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[[types.KeyboardButton("/reset")]]))
+    if message.text == "/reset":
+        selected_procedure = None
+        selected_place = None
+        selected_date_time = None
+        user_phone = None
+        # # Сброс остальных сохраненных данных, если есть
+        await state.reset_state()
         await state.finish()
+        await message.answer("Настройки сброшены. Вы можете начать процесс заново, нажав на кнопку /start.",
+                             reply_markup=types.ReplyKeyboardMarkup(
+                                 resize_keyboard=True,
+                                 keyboard=[
+                                     [types.KeyboardButton("/start")]
+                                 ]
+                             ))
+
+
+# @dp.message_handler(commands=['reset'])                #(commands=['reset'])
+# async def reset_settings(message: types.Message):
+    # код для сброса настроек пользователя
+    # например, сброс состояний FSMContext или удаление сохраненных данных о пользователе
+
+
+  
+
+
+
 # @dp.message_handler()
 # async def echo(message: types.Message):
 #     await message.answer('Пожалуйста, выберите параметры с помощью кнопок. \nЕсли кнопок не видно - нажмите на квадратную кнопку с 4 точками')     #message.text
